5fae6c424243b40a8152f943fe376dda9d4b8274
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2020-12-03 22:25:35 +0200

    Update config.inc.php

--- Admin Panel/include/config.inc.php
+++ Admin Panel/include/config.inc.php
@@ -14,8 +14,8 @@
 
 
 $SITE_NAME="Welcome to AME";
-$SITE_URL="https://tikers.tech/Admin%20Panel/adminpanel/";
-$DBSERVER = "tikers.tech";
+$SITE_URL="http://tikers.tech/Admin_Panel/adminpanel/";
+$DBSERVER = "199.247.14.102";
 $DATABASENAME = "udqzzzmwub";
 $USERNAME = "udqzzzmwub";
 $PASSWORD = "v77G5ERqYX";
