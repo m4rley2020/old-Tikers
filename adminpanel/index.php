4cbfcd8d141ce202e0c0ee452251ce4fc031707b
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2020-12-04 12:19:14 +0200

    eh

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -1,18 +1,17 @@
 <?php 
 
-    include ("../include/config.inc.php");
+    // include ("../include/config.inc.php");
 	// include_once ("../include/sendmail.php");
-	//include ("../include/functions.php");
-	//$pas;
+	// include ("../include/functions.php");
+	$pas;
   $ADMIN_MOUSEHOUR_COLOUR="#cccccc";
   $ADMIN_MOUSEOUT_COLOUR="#FFFFFF";
   $ADMIN_TOP_BGCOLOUR="#FFFFFF";
   
-   $db=mysql_connect($DBSERVER, $USERNAME, $PASSWORD);
-   mysql_select_db($DATABASENAME,$db);  
-   $pas=$_GET["pas"];
-   //$pas="";
-
+//   $db=mysql_connect($DBSERVER, $USERNAME, $PASSWORD);
+//   mysql_select_db($DATABASENAME,$db);  
+// $pas=$_GET["pas"];
+$pas="";
 ?>
 <!DOCTYPE html>
 <html lang="en"> 
