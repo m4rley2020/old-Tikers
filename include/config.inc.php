ba50648e8bfe7bcaa252cf3d15575c3c06317b9d
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2020-12-05 14:16:33 +0200

    Update config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -3,12 +3,12 @@
 error_reporting(0);
 
 $SITE_NAME="Welcome to AME";
-$SITE_URL="http://keshavinfotechdemo.com/KESHAV/KG1/AME/";
-$DBSERVER = "db530831774.db.1and1.com";
-$DATABASENAME = "db530831774";
-$USERNAME = "dbo530831774";
-$PASSWORD = "Keshav_777";
-$ADMIN_MAIL="admin@gmail.com.com";
+$SITE_URL="http://tikers.tech/adminpanel/";
+$DBSERVER = "199.247.14.102";
+$DATABASENAME = "ghadxjbzcn";
+$USERNAME = "ghadxjbzcn";
+$PASSWORD = "EywGxuNez4";
+$ADMIN_MAIL="admin@gmail.com";
 $SITE_TITLE="AME";
 $KFCK = "../../";
 ?>
