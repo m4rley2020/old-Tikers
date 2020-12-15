1a703fb9f4f58afe98b32b0221a72de06c6c42ac
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2020-12-15 11:46:40 +0200

    all challenges

---
+++ webservice/get_all_challenge_list.php
@@ -0,0 +1,90 @@
+<?php
+	header("Content-type: application/json");
+	include("connect.php");
+
+	
+	if($_REQUEST['user_id'] != '' && $_REQUEST['latitude'] != '' && $_REQUEST['longitude'] != '')
+	{
+		
+		$latitude = $_REQUEST['latitude'];
+		$longitude = $_REQUEST['longitude'];
+
+		$get_query = "select * from store_challenges";
+		
+		/*if($_REQUEST['challenge_type_id'] != '')
+		{
+			$get_query .= " and challenge_type_id = ".$_REQUEST['challenge_type_id']." ";
+		}*/
+		
+		
+		
+		
+		
+		
+		
+		$result = mysqli_query($db,$get_query);
+
+		if(mysqli_num_rows($result) > 0)
+		{
+			while($row = mysqli_fetch_array($result))
+			{
+				$challenge_image = $row['challeng_image'];
+				
+				
+
+				$get_query3 = "select name,rating from store where id = '".$row['store_id']."'";		
+					$get_query_res3 =   mysqli_query($db,$get_query3)or die(mysqli_error($db));
+					
+					if(mysqli_num_rows($get_query_res3)>0)
+					{							
+						while($get_query_data3 = mysqli_fetch_array($get_query_res3))
+						{
+							$store_name = $get_query_data3['name'];
+							$store_rating = number_format($get_query_data3['rating'],2);
+						}
+					}
+
+				if(file_exists("../challenge_image/".$challenge_image) && $challenge_image!="")
+				{
+					$challenge_image1 = $SITE_URL."../challenge_image/".$challenge_image;
+				}
+				else
+				{
+					$challenge_image1 = "0";
+				}
+
+				$data[]=array(
+							"challenge_id" => $row['id'], 
+							"storename" => $store_name,
+							"challengename" => $row['name'],
+							"image" => $challenge_image1,
+							"rating" => $row['rating'],						
+							"storeid" => $row['store_id'],
+							"lattitude" => $row['lattitude'],
+							"longitude" => $row['longitude'],
+							"location" => $row['location'],
+							"points" => $row['points'],
+							"description" => $row['description'],
+							"rating" => $store_rating,
+							"counter"=>$row['counter']
+							);
+			}
+
+			$error = "Success";
+			$result=array('message'=> $error, 'result'=>'1','responseData'=>$data);	
+		}
+		else
+		{
+			$error = "Challenge not found.";
+			$result=array('message'=> $error, 'result'=>'0');
+		}
+	}
+	else
+	{
+		$error = "Please enter all required fields";
+		$result=array('message'=> $error, 'result'=>'0');
+	}
+	
+	$result=json_encode($result);
+	echo $result;
+?>
--- webservice/user_complete_challenge.php
+++ webservice/user_complete_challenge.php
@@ -2,7 +2,7 @@
 	header("Content-type: application/json");
 	include("connect.php");
 			 
-	if($_REQUEST['user_id'] != '' && $_REQUEST['challenge_id'] != '' && $_REQUEST['store_id'] != '' && $_REQUEST['status'] != '' && $_REQUEST['amount'] != '' && $_REQUEST['currency'] != '')
+	if($_REQUEST['user_id'] != '' && $_REQUEST['challenge_id'] != '' && $_REQUEST['store_id'] != '' )
 	{
 		
 		if($_FILES['challenge_image']['name']!="")
@@ -10,30 +10,31 @@
 			$challenge_image = str_replace(" ","_",rand(1,999).trim($_FILES['challenge_image']['name']));
 			move_uploaded_file($_FILES["challenge_image"]["tmp_name"],"../complete_challenge_image/".$challenge_image);
 		}
+
+		$counter;
+		$q1 = "select counter from store_challenges where id='".$_REQUEST['challenge_id']."'";
 		
-		if($_FILES['bill_image']['name']!="")
-		{	
-			$bill_image = str_replace(" ","_",rand(1,999).trim($_FILES['bill_image']['name']));
-			move_uploaded_file($_FILES["bill_image"]["tmp_name"],"../complete_challenge_image/".$bill_image);
-		}
-		
-		if($_REQUEST['status'] == "public")
+		$get_query_res2 = mysqli_query($db,$q1)or die(mysqli_error($db));
+
+		if(mysqli_num_rows($get_query_res2)>0)
 		{
-			$status = 1;
+			while($get_query_date2 = mysqli_fetch_array($get_query_res2))
+			{$counter = $get_query_date2['counter'];
+
+			}
+			$counter++;
 		}
-		else
-		{
-			$status = 0;
-		}
+		$insert_query1 = "update store_challenges  set counter ='".$counter."' where id='".$_REQUEST['challenge_id']."'";
+		mysqli_query($db,$insert_query1)or die(mysqli_error($db));
+
+				
+				$error = "Challenge Completed Successfully";
+				$result=array('message'=> $error, 'result'=>'1');
 		
 		$insert_query = "insert into store_challenge_complete_by_user set 					
 				user_id='".$_REQUEST['user_id']."',
 				challenge_id='".$_REQUEST['challenge_id']."',
 				store_id='".$_REQUEST['store_id']."',
-				status='".$status."',
-				amount='".$_REQUEST['amount']."',
-				currency='".$_REQUEST['currency']."',
-				bill_image='".$bill_image."',
 				challenge_image='".$challenge_image."',				
 				add_date = NOW()";
 				mysqli_query($db,$insert_query)or die(mysqli_error($db));
