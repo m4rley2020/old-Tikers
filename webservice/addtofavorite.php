9262a16d2f432feb4baf136cb18059a998c84ab7
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2020-12-23 21:54:41 +0200

    Update addtofavorite.php

--- webservice/addtofavorite.php
+++ webservice/addtofavorite.php
@@ -2,27 +2,27 @@
 	header("Content-type: application/json");
 	include("connect.php");
 			 
-	if($_REQUEST['user_id'] != '' && $_REQUEST['challange_id'] != '')
+	if($_REQUEST['user_id'] != '' && $_REQUEST['challenge_id'] != '')
 	{
 		
-		$get_query = "select * from favourite where user_id = '".$_REQUEST['user_id']."' and challange_id = '".$_REQUEST['challange_id']."'";		
+		$get_query = "select * from favourite where user_id = '".$_REQUEST['user_id']."' and challenge_id = '".$_REQUEST['challenge_id']."'";		
 		$get_query_res =   mysqli_query($db,$get_query)or die(mysqli_error($db));
 		
 		if(mysqli_num_rows($get_query_res)>0)
 		{
-			$error = "Challange is alredy added.";
+			$error = "Challenge is alredy added.";
 			$result=array('message'=> $error, 'result'=>'0');
 		}
 		else
 		{
 		
-		$challange_id = $_REQUEST['challange_id'];
+		$challenge_id = $_REQUEST['challenge_id'];
 		
-		$store_id = GetValue("store_challenges","store_id","id",$challange_id);
+		$store_id = GetValue("store_challenges","store_id","id",$challenge_id);
 
 		$insert_query = "insert into favourite set 					
 				user_id='".$_REQUEST['user_id']."',
-				challange_id='".$_REQUEST['challange_id']."',
+				challenge_id='".$_REQUEST['challenge_id']."',
 				add_date=NOW(),				
 				store_id = '".$store_id."'";
 				mysqli_query($db,$insert_query)or die(mysqli_error($db));
