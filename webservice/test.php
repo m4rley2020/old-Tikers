aaa99db0d6ca5e948eaadfdbdc30c5f6c08ba835
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2021-01-01 22:12:23 +0200

    Update test.php

--- webservice/test.php
+++ webservice/test.php
@@ -10,15 +10,17 @@
 			move_uploaded_file($_FILES["challenge_image"]["tmp_name"],"../complete_challenge_image/".$challenge_image);
 		}
 
-		$points = GetValue("user","points","user_id",$_REQUEST['user_id'],$db);
-		echo $points;
+		$get_query4 = "select points from user where id = '" . $_REQUEST['user_id'] . "'";
+	$get_query_res4 =   mysqli_query($db, $get_query4) or die(mysqli_error($db));
+	$get_q4 = mysqli_fetch_array($get_query_res4);
+	$user_points = $get_q4['points'];
 
 		$insert_query = "insert into store_challenge_complete_by_user set 					
 				user_id='".$_REQUEST['user_id']."',
 				challenge_id='".$_REQUEST['challenge_id']."',
 				store_id='".$_REQUEST['store_id']."',
 				challenge_image='".$challenge_image."',	
-				points='".$points."',	
+				points='".$user_points."',	
 				add_date = NOW()";
 				mysqli_query($db, $insert_query)or die(mysqli_error($db));
 				$post_id = mysqli_insert_id($db);
