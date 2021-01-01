d955d76a994ac74577b4e843dc7a34bd2d28c66a
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2021-01-01 22:02:38 +0200

    Update test.php

--- webservice/test.php
+++ webservice/test.php
@@ -10,9 +10,6 @@
 			move_uploaded_file($_FILES["challenge_image"]["tmp_name"],"../complete_challenge_image/".$challenge_image);
 		}
 
-		$points = GetValue("user","points","user_id",$_REQUEST['user_id']);
-		echo $points;
-
 		$insert_query = "insert into store_challenge_complete_by_user set 					
 				user_id='".$_REQUEST['user_id']."',
 				challenge_id='".$_REQUEST['challenge_id']."',
