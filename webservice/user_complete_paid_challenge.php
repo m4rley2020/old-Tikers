3e84dcde3b25946fdfc5b182aeafbb46d8783349
ameenhaj <75387352+ameenhaj@users.noreply.github.com>
2021-02-27 17:15:59 +0200

    Update user_complete_paid_challenge.php

--- webservice/user_complete_paid_challenge.php
+++ webservice/user_complete_paid_challenge.php
@@ -10,7 +10,7 @@
 			move_uploaded_file($_FILES["challenge_image"]["tmp_name"],"../complete_challenge_image/".$challenge_image);
 		}
 		
-		$code_query= "select * from store_code where code ='".$_REQUEST['code']."' AND is_used = 'No' & store_id = '".$_REQUEST['store_id']."'";
+		$code_query= "select * from store_code where code ='".$_REQUEST['code']."' AND is_used = 'No' & store_id ='".$_REQUEST['store_id']."'";
 		$get_code= mysqli_query($db,$code_query)or die(mysqli_error($db));
 
 		/* ---------------------- check code and get user points ---------------------- */
