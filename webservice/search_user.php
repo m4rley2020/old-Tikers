995d70e66976b6699e103f4ca81507d7e081eb19
HadiDagher96 <HadiDagher96@users.noreply.github.com>
2021-03-02 16:02:08 +0200

    Update search_user.php

--- webservice/search_user.php
+++ webservice/search_user.php
@@ -1,76 +1,102 @@
 <?php
-	header("Content-type: application/json");
-	include("connect.php");
-			 
-	if($_REQUEST['user_id'] != '' && $_REQUEST['searchname'])
-	{
-		
-		$get_query = "select * from user where username like '%".$_REQUEST['searchname']."%' and id != '".$_REQUEST['user_id']."'";
-		$get_query_res =   mysqli_query($db,$get_query)or die(mysqli_error($db));
-		
-		$get_query1 = "select * from store where name like '%".$_REQUEST['searchname']."%' and user_id != '".$_REQUEST['user_id']."'";
-		$get_query_res1 =   mysqli_query($db,$get_query1)or die(mysqli_error($db));
-		
-		if(mysqli_num_rows($get_query_res)>0)
-		{
-			while($get_query_date = mysqli_fetch_array($get_query_res))
-			{
-				$user_id = $get_query_date['id'];
-				$username = $get_query_date['username'];
-				$first_name = $get_query_date['first_name'];
-				$last_name = $get_query_date['last_name'];				
-				$profile_image = $get_query_date['profile_image'];
-				$is_private = $get_query_date['is_private'];
-				
-				if(file_exists("../User_image/".$profile_image) && $profile_image!="")
-				{
-					$profile_imagel = $SITE_URL."/User_image/".$profile_image;
-				}
-				else
-				{
-					$profile_imagel = "";
-				}
-				
-				// 0 = follow
-				// 1 = requested
-				// 2 = following
-				$is_follow = "0";
-				$check_follow = mysqli_query($db,"select status from friend where from_user = '".$_REQUEST['user_id']."' and to_user = '".$user_id."' ") or die(mysqli_error($db)) ;
-				
-				if(mysqli_num_rows($check_follow)>0)
-				{
-					$follow_data = mysqli_fetch_array($check_follow);
-					$is_follow = $follow_data['status'];
-				}
-				
-			
-				$data[]=array(
-						"user_id"=>$user_id, 
-						"username"=>$username,
-						"first_name"=>$first_name,
-						"last_name"=>$last_name,
-						"profile_image"=>$profile_imagel,
-						"is_follow"=>$is_follow,
-						"is_private"=>$is_private,
-						);
-				
+header("Content-type: application/json");
+include("connect.php");
+
+if ($_REQUEST['user_id'] != '' && $_REQUEST['searchname']) {
+
+	$get_query = "select * from user where username like '%" . $_REQUEST['searchname'] . "%' and id != '" . $_REQUEST['user_id'] . "'";
+	$get_query_res =   mysqli_query($db, $get_query) or die(mysqli_error($db));
+
+	if (mysqli_num_rows($get_query_res) > 0) {
+		while ($get_query_date = mysqli_fetch_array($get_query_res)) {
+			$user_id = $get_query_date['id'];
+			$username = $get_query_date['username'];
+			$first_name = $get_query_date['first_name'];
+			$last_name = $get_query_date['last_name'];
+			$profile_image = $get_query_date['profile_image'];
+			$is_private = $get_query_date['is_private'];
+
+			if (file_exists("../User_image/" . $profile_image) && $profile_image != "") {
+				$profile_imagel = $SITE_URL . "/User_image/" . $profile_image;
+			} else {
+				$profile_imagel = "";
 			}
-			$message="User found.";
-			$result=array('message'=> $message, 'result'=>'1','responseData'=>$data);	
-			
+
+			// 0 = follow
+			// 1 = requested
+			// 2 = following
+			$is_follow = "0";
+			$check_follow = mysqli_query($db, "select status from friend where from_user = '" . $_REQUEST['user_id'] . "' and to_user = '" . $user_id . "' ") or die(mysqli_error($db));
+
+			if (mysqli_num_rows($check_follow) > 0) {
+				$follow_data = mysqli_fetch_array($check_follow);
+				$is_follow = $follow_data['status'];
+			}
+
+
+			$data[] = array(
+				"user_id" => $user_id,
+				"username" => $username,
+				"first_name" => $first_name,
+				"last_name" => $last_name,
+				"profile_image" => $profile_imagel,
+				"is_follow" => $is_follow,
+				"is_private" => $is_private,
+			);
 		}
-		else
-		{
-			$error = "User not found.";
-			$result=array('message'=> $error, 'result'=>'0');
-		}
-		
+		$message = "User found.";
+		$result = array('message' => $message, 'result' => '1', 'responseData' => $data);
 	}
-	else
-	{
-		$error = "Please enter all required fields";
-		$result=array('message'=> $error, 'result'=>'0');
-	}	
-	$result=json_encode($result);
-	echo $result;
-?>
+
+	/** --------------------------------------------------------------------- */
+
+	$get_query1 = "select * from store where name like '%" . $_REQUEST['searchname'] . "%'";
+	$get_query_res1 =   mysqli_query($db, $get_query1) or die(mysqli_error($db));
+
+	if (mysqli_num_rows($get_query_res1) > 0) {
+		while ($get_query_data = mysqli_fetch_array($get_query_res1)) {
+			$user_id = $get_query_data['id'];
+			$username = $get_query_data['name'];
+			$first_name = $get_query_data['gender'];
+			$last_name = $get_query_data['rating'];
+			$profile_image = $get_query_data['store_image'];
+
+			if (file_exists("../store_image/" . $profile_image) && $profile_image != "") {
+				$profile_imagel = $SITE_URL . "/store_image/" . $profile_image;
+			} else {
+				$profile_imagel = "";
+			}
+
+			// 0 = follow
+			// 1 = requested
+			// 2 = following
+			$is_follow = "0";
+			$check_follow = mysqli_query($db, "select status from friend where from_user = '" . $_REQUEST['user_id'] . "' and to_user = '" . $user_id . "' ") or die(mysqli_error($db));
+
+			if (mysqli_num_rows($check_follow) > 0) {
+				$follow_data = mysqli_fetch_array($check_follow);
+				$is_follow = $follow_data['status'];
+			}
+
+
+			$data[] = array(
+				"user_id" => $user_id,
+				"username" => $username,
+				"first_name" => $first_name,
+				"last_name" => $last_name,
+				"profile_image" => $profile_imagel,
+				"is_follow" => $is_follow,
+			);
+		}
+		$message = "User found.";
+		$result = array('message' => $message, 'result' => '1', 'responseData' => $data);
+	} else {
+		$error = "User not found.";
+		$result = array('message' => $error, 'result' => '0');
+	}
+} else {
+	$error = "Please enter all required fields";
+	$result = array('message' => $error, 'result' => '0');
+}
+$result = json_encode($result);
+echo $result;
